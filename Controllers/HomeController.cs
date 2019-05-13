dd9abf6ae045b2a8d77a0d0e83ed6d583a752d1b
Patrick Wood <patrick.wood@phoenixrcs.org>
2019-05-12 21:37:14 -0400

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Model;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IJsReportMVCService JsReportMVCService { get; }
+
+        public HomeController(IJsReportMVCService jsReportMVCService)
+        {
+            JsReportMVCService = jsReportMVCService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Invoice()
+        {
+            HttpContext.JsReportFeature().Recipe(Recipe.ChromePdf);
+
+            return View(InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult InvoiceDownload()
+        {
+            HttpContext.JsReportFeature().Recipe(Recipe.ChromePdf)
+                .OnAfterRender((r) => HttpContext.Response.Headers["Content-Disposition"] = "attachment; filename=\"myReport.pdf\"");
+
+            return View("Invoice", InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public async Task<IActionResult> InvoiceWithHeader()
+        {
+            var header = await JsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Header", new { });
+
+            HttpContext.JsReportFeature()
+                .Recipe(Recipe.ChromePdf)
+                .Configure((r) => r.Template.Chrome = new Chrome {
+                    HeaderTemplate = header,
+                    DisplayHeaderFooter = true,
+                    MarginTop = "5cm",
+                    MarginLeft = "1cm",
+                    MarginBottom = "1cm",
+                    MarginRight = "1cm"
+                });
+
+            return View("Invoice", InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Items()
+        {
+            HttpContext.JsReportFeature()
+                .Recipe(Recipe.HtmlToXlsx)
+                .Configure((r) => r.Template.HtmlToXlsx = new HtmlToXlsx() { HtmlEngine = "chrome" });
+
+            return View(InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult ItemsExcelOnline()
+        {
+            HttpContext.JsReportFeature()
+                .Configure(req => req.Options.Preview = true)
+                .Recipe(Recipe.HtmlToXlsx)
+                .Configure((r) => r.Template.HtmlToXlsx = new HtmlToXlsx() { HtmlEngine = "chrome" });
+
+            return View("Items", InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult InvoiceDebugLogs()
+        {
+            HttpContext.JsReportFeature()
+                .DebugLogsToResponse()
+                .Recipe(Recipe.ChromePdf);
+
+            return View("Invoice", InvoiceModel.Example());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using jsreport.AspNetCore;
+using jsreport.Local;
+using jsreport.Binary;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()                
+                .Build();
+
+            host.Run();
+        }
+    }
+}
